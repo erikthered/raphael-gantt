790a46db22a2b6d55e08c4f51976f7318155359a
Erik Nelson <erik.david.nelson@gmail.com>
2012-06-27 16:55:30 -0400

    Changed over to using class properties and methods

--- raphael-gantt.js
+++ raphael-gantt.js
@@ -1,16 +1,11 @@
-var height = 500;
-var width = 500;
-var gridSize = 25;
-
-var labelAreaSize = 100;
-
-// Starts at 1 so there is an empty row for headers
-var currentRow = 1;
-
-var paper;
-
 function GanttChart(elementId){
   this.elementId = elementId;
+  this.height = 500;
+  this.width = 500;
+  this.gridSize = 25;
+  this.labelAreaSize = 100;
+  this.currentRow = 1; // Starts at 1 so there is an empty row for headers
+
 
   this.loadData = function(payload){
     this.phases = payload;
@@ -18,36 +13,37 @@ function GanttChart(elementId){
 
   this.draw = function(){
     // Get the list of dates for the project duration
-    var dates = getDates();
+    var dates = this.getDates();
     // Calculate total graph width
-    width = labelAreaSize + dates.length * gridSize;
+    width = this.labelAreaSize + dates.length * this.gridSize;
     var rows = this.getRows();
-    height = rows*gridSize;
+    height = rows*this.gridSize;
 
     // Generate the chart area
-    paper = Raphael(this.elementId, width, height);
+    this.paper = Raphael(this.elementId, width, height);
 
-    grid();
-    headers(dates);
+    this.grid();
+    this.headers(dates);
 
     // Iterate through all the phases, print labels and child tasks
     var phases = this.phases;
     for(p in phases){
       //TODO print phase bounds
-      var caption = paper.text(5,(currentRow*gridSize)+(gridSize/2),phases[p].name);
+      var caption = this.paper.text(5,(this.currentRow*this.gridSize)+(this.gridSize/2),phases[p].name);
       caption.attr({"font-size":14 , "stroke":"none" , "fill":"black", "text-anchor":"start"});
 
-      currentRow++;
+      this.currentRow++;
 
       var tasks = phases[p].tasks;
       for(t in tasks){
-        bar(tasks[t]);
-        currentRow++;
+        this.bar(tasks[t]);
+        this.currentRow++;
       }
     }
 
   }
 
+  // Gets the total number of rows
   this.getRows = function(){
     var phases = this.phases;
     var rows = 1;
@@ -62,40 +58,40 @@ function GanttChart(elementId){
   }
 
   // Draw the grid
-  function grid(){
+  this.grid = function(){
     // Draw horizontal grid lines
-    for(var y=gridSize; y < height; y+=gridSize){
+    for(var y=this.gridSize; y < height; y+=this.gridSize){
       var pathExp = "M0," + y + "L" + width + "," + y;
-      var gridLine = paper.path(pathExp);
+      var gridLine = this.paper.path(pathExp);
       gridLine.attr({"stroke":"#000"});
     }
 
     //Draw vertical grid lines (starts at the edge of the space allotted for phase/task labels)
-    for(var x=labelAreaSize; x < width; x+=gridSize){
+    for(var x=this.labelAreaSize; x < width; x+=this.gridSize){
       var pathExp = "M" + x + ",0L" + x + "," + height;
-      var gridLine = paper.path(pathExp);
+      var gridLine = this.paper.path(pathExp);
     }
   }
 
   // Draws column headers, currently using M-D formatting
-  function headers(dates){
+  this.headers = function(dates){
     var currentCol = 0;
     for(d in dates){
-      var heading = paper.text(labelAreaSize + currentCol*gridSize + .5*gridSize, .5*gridSize,dates[d].format("M-D"));
+      var heading = this.paper.text(this.labelAreaSize + currentCol*this.gridSize + .5*this.gridSize, .5*this.gridSize,dates[d].format("M-D"));
       heading.attr({"text-anchor":"middle"});
       currentCol++;
     }
   }
 
   // Draws a task bar
-  function bar(task){
-    var x = task.startOffset*gridSize+labelAreaSize;
-    var y = currentRow*gridSize;
-    var barWidth = task.length*gridSize;
-    var barHeight = gridSize;
+  this.bar = function(task){
+    var x = task.startOffset*this.gridSize+this.labelAreaSize;
+    var y = this.currentRow*this.gridSize;
+    var barWidth = task.length*this.gridSize;
+    var barHeight = this.gridSize;
 
-    var bar = paper.rect(x, y, barWidth, barHeight);
-    var caption = paper.text(10,y+(barHeight/2),task.name);
+    var bar = this.paper.rect(x, y, barWidth, barHeight);
+    var caption = this.paper.text(10,y+(barHeight/2),task.name);
     caption.attr({"font-size":14 , "stroke":"none" , "fill":"black", "text-anchor":"start"});
 
     bar.attr({"fill":"#000"});
@@ -108,7 +104,7 @@ function GanttChart(elementId){
   }
 
   // Returns an array of dates for the duration of the project
-  function getDates(){
+  this.getDates = function(){
     //TODO Parse start/end dates from data
     var startDate = moment();
     var endDate = startDate.clone();
